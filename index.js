c96259fa04aef59d21be571004039e99c91eac37
Ali Kamal <alikamal1@yahoo.com>
2020-01-18 13:07:34 +0300

    comments

--- index.js
+++ index.js
@@ -6,18 +6,21 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', function(socket) {
     console.log('a user connected');
 
-    socket.on('chat-message', function(msg) {
-        io.emit('chat-message', msg);
+    // server received new message event
+    socket.on('new-message', function(msg) {
+        // server broadcast new message event to clients
+        io.emit('new-message', msg);
         console.log('message: ' + msg);
     });
 
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
-})
+});
 
 http.listen(3000, function() {
     console.log('Listening on *:3000');
